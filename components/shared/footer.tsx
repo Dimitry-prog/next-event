728ac66a6f46fde8aa0b38ceaee3b372db592138
Dmitry <kfifa3@gmail.com>
2023-12-31 18:11:24 +0300

    feat: header and footer ui

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,7 +1,21 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 const Footer = () => {
   return (
-    <footer>
-      Footer
+    <footer className='border-t'>
+      <div className='wrapper flex-between flex-col gap-4 text-center sm:flex-row'>
+        <Link href='/'>
+          <Image
+            src='/assets/images/logo.svg'
+            alt='Evently logo'
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <p>2024 Event. All Rights reserved.</p>
+      </div>
     </footer>
   );
 };
--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,7 +1,23 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 const Header = () => {
   return (
-    <header>
-      Header
+    <header className='w-full border-b'>
+      <div className='wrapper flex-between '>
+        <Link href='/' className='w-36'>
+          <Image
+            src='/assets/images/logo.svg'
+            alt='Evently logo'
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <div className='w-32 flex justify-end gap-3'>
+          auth
+        </div>
+      </div>
     </header>
   );
 };
