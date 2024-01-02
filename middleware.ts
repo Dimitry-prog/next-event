a085ea5143a6e275b9b3e8c5d64e11a629885428
Dmitry <kfifa3@gmail.com>
2024-01-02 21:21:40 +0300

    fix: deploy issue

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,7 @@ export default authMiddleware({
     // '/api/webhook/clerk',
     // '/api/webhook/stripe',
     '/api/webhooks(.*)',
+    '/api/webhook',
     '/api/uploadthing',
   ],
   // ignoredRoutes: ['/api/webhook/clerk', '/api/webhook/stripe', '/api/uploadthing'],
