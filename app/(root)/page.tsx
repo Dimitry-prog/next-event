868b8d1c2385ed5f578fc320b49884eebc72035c
Dmitry <kfifa3@gmail.com>
2024-01-02 15:23:33 +0300

    feat: home ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,41 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import Image from 'next/image';
+
 export default function Home() {
-  return <section>Home</section>;
+  return (
+    <>
+      <section className="py-5 md:py-10 bg-primary-50 bg-dotted-pattern bg-contain">
+        <div className="wrapper grid grid-cols-1 md:grid-cols-2 gap-5 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">Create, Invite: Your Events, Here!</h1>
+
+            <p className="p-reqular-20 md:p-regular-24">
+              Learn and book helpful tips from ours mentors across world
+            </p>
+
+            <Button size="lg" asChild className="w-full sm:w-fit button">
+              <Link href="#events">Explore now</Link>
+            </Button>
+          </div>
+
+          <Image
+            src="/assets/images/hero.png"
+            alt="hero"
+            width={1000}
+            height={1000}
+            className="max-h-[70vh] 2xl:max-h-[50vh] object-contain object-center"
+          />
+        </div>
+      </section>
+
+      <section id="events" className="my-8 wrapper flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">
+          Trust by <br /> Thousands of Events
+        </h2>
+
+        <div className="w-full flex flex-col md:flex-row gap-5">Search Category</div>
+      </section>
+    </>
+  );
 }
